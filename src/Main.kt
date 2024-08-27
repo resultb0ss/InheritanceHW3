7051dc3ee4396d3d194f985e89863cc35742a83d
Admin <reteamboss@gmail.com>
2024-08-27 21:29:05 +0300

    Задание 3

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,8 @@
 fun main() {
-    println("Hello World!")
+    var array = intArrayOf(3,67,1,55,65,89,23)
+    for (num in array){
+        if (num %  5 == 0){
+            println(num)
+        }
+    }
 }
